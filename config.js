958eace74452767822c0799812f9e0a490b3574f
Kocsis Lorand <kocsislorand98@gmail.com>
2018-01-26 18:21:39 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
     config: {
-        tokken: "MzQ4NzcxNTAzMzU5MTMxNjQ5.DPWu-g.WFzS8Q-bx8TuxZAObsmuEOjiN0Y",
+        tokken: "",
         prefix: "g! ",
-        imgur: "c54367c7b0b798e",
-        movie: "2631951f11e5458dade8e478a363570f",
-        weather: "649e218e7990eb5269334f0ca9adf2d8",
-        youtube: "AIzaSyD1J1tnUlAxa0WxO9-XY6AwsuoBc7Dso1w"
+        imgur: "",
+        movie: "",
+        weather: "",
+        youtube: ""
     }
 }
